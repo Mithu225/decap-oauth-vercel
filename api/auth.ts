664efc0e58a2c32337bf795a4dce418fee65df5d
Mithu225 <thu@opendigital.no>
2024-03-13 21:12:49 +0100

    add logs

--- api/auth.ts
+++ api/auth.ts
@@ -28,4 +28,4 @@ export default function handler(req: VercelRequest, res: VercelResponse) {
   }
 }
 
-export const runtime = "experimental-edge";
+export const runtime = "edge";
--- api/callback.ts
+++ api/callback.ts
@@ -63,4 +63,4 @@ export default async function handler(req: VercelRequest, res: VercelResponse) {
   }
 }
 
-export const runtime = "experimental-edge";
+export const runtime = "edge";
