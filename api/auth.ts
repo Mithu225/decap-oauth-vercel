039f07968e82da66b59c442f3120ef7441df0271
Mithu225 <thu@opendigital.no>
2024-03-13 20:33:50 +0100

    init

---
+++ api/auth.ts
@@ -0,0 +1,23 @@
+import type { VercelRequest, VercelResponse } from "@vercel/node";
+
+export default function handler(req: VercelRequest, res: VercelResponse) {
+  const client_id = process.env.GITHUB_CLIENT_ID;
+
+  try {
+    const url = new URL(req.url!);
+    const redirectUrl = new URL("https://github.com/login/oauth/authorize");
+    redirectUrl.searchParams.set("client_id", client_id!);
+    redirectUrl.searchParams.set("redirect_uri", url.origin + "/api/callback");
+    redirectUrl.searchParams.set("scope", "repo user");
+    redirectUrl.searchParams.set(
+      "state",
+      crypto.getRandomValues(new Uint8Array(12)).join("")
+    );
+    return res.redirect(301, redirectUrl.href);
+  } catch (error) {
+    console.error(error);
+    return res.status(500).json({
+      message: error.message,
+    });
+  }
+}
